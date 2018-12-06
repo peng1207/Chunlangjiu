20d36119dcb8670a9e325c81360331c51cba41f2
chengzong liang <chengzongliang@chengzongdeMac-mini.local>
2018-12-06 16:21:01 +0800

    增加设置 资金管理 粉丝 活动 设置 目录

--- Chunlangjiu/Community/ViewController/SPTutorialPageVC.swift
+++ Chunlangjiu/Community/ViewController/SPTutorialPageVC.swift
@@ -23,7 +23,8 @@ class SPTutorialPageVC : SPBaseVC{
         btn.addTarget(self, action: #selector(sp_clickAdvert), for: UIControlEvents.touchUpInside)
         return btn
     }()
-    
+    fileprivate let tutorialPageCellID = "tutorialPageCellID"
+    fileprivate lazy var imgArray : [UIImage] = []()
     override func viewDidLoad() {
         super.viewDidLoad()
         sp_setupUI()
@@ -48,12 +49,31 @@ class SPTutorialPageVC : SPBaseVC{
     }
     /// 添加UI
     override func sp_setupUI(){
-        
+        let layout = UICollectionViewFlowLayout()
+        layout.minimumLineSpacing = 0
+        layout.minimumInteritemSpacing = 0
+        layout.scrollDirection = .horizontal
+        layout.sectionInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+        self.collectionView = UICollectionView(frame: CGRect.zero, collectionViewLayout: layout)
+        self.collectionView.delegate = self
+        self.collectionView.dataSource = self
+        self.collectionView.backgroundColor = UIColor.white
+        self.collectionView.register(SPTutorialPageCollectCell.self, forCellWithReuseIdentifier: tutorialPageCellID)
+        self.view.addSubview(self.collectionView)
+        self.view.addSubview(self.skipBtn)
         self.sp_addConstraint()
     }
     /// 添加约束
     fileprivate func sp_addConstraint(){
-        
+        self.collectionView.snp.makeConstraints { (maker) in
+            maker.left.right.top.bottom.equalTo(self.view).offset(0)
+        }
+        self.skipBtn.snp.makeConstraints { (maker) in
+            maker.height.equalTo(30)
+            maker.width.equalTo(60)
+            maker.right.equalTo(self.view.snp.right).offset(-20)
+            maker.top.equalTo(self.view.snp.top).offset(sp_getstatusBarHeight() + 20)
+        }
     }
     /// 处理数据
     fileprivate func sp_setupData(){
@@ -63,5 +83,46 @@ class SPTutorialPageVC : SPBaseVC{
 extension SPTutorialPageVC {
     @objc fileprivate func sp_clickAdvert(){
         
+    }
+}
+extension SPTutorialPageVC : UICollectionViewDelegate,UICollectionViewDataSource,UICollectionViewDelegateFlowLayout{
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return sp_getArrayCount(array: self.imgArray) > 0 ? 1 : 0
+    }
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return sp_getArrayCount(array: self.imgArray)
+    }
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell : SPTutorialPageCollectCell = collectionView.dequeueReusableCell(withReuseIdentifier: tutorialPageCellID, for: indexPath) as! SPTutorialPageCollectCell
+        if  indexPath.row < sp_getArrayCount(array: self.imgArray) {
+            cell.imageView.image = self.imgArray[indexPath.row]
+        }
+        return cell
+    }
+    
+}
+
+
+class SPTutorialPageCollectCell : UICollectionViewCell{
+    lazy var imageView : UIImageView = {
+        return UIImageView()
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        sp_setupUI()
+    }
+    
+    /// 添加UI
+    override func sp_setupUI(){
+        self.contentView.addSubview(self.imageView)
+        sp_addConstraint()
+    }
+    
+    /// 添加约束
+    fileprivate func sp_addConstraint(){
+        self.imageView.snp.makeConstraints { (maker) in
+            maker.left.right.top.bottom.equalTo(self.contentView).offset(0)
+        }
     }
 }
