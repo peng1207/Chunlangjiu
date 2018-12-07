bae50d631f9380ff3c1207f55d28c29cc7412b35
chengzong liang <chengzongliang@chengzongdeMac-mini.local>
2018-12-07 17:54:20 +0800

    增加类

---
+++ Chunlangjiu/APP/Set/Model/SPSetModel.swift
@@ -0,0 +1,13 @@
+//
+//  SPSetModel.swift
+//  Chunlangjiu
+//
+//  Created by chengzong liang on 2018/12/7.
+//  Copyright © 2018年 Chunlang. All rights reserved.
+//
+
+import Foundation
+class SPSetModel: SPBaseModel {
+    var name : String?
+    
+}
